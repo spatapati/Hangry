6a78abe33c4ddffd1c45ec4c357395c71bc4f7eb
abhaved <abhavedula@gmail.com>
2015-07-29 10:31:33 -0400

    Update model.rb

--- apps/models/model.rb
+++ apps/models/model.rb
@@ -1,11 +1,33 @@
-# this creates a new connection to the Yummly API
-# it should go in an initialize method in your new model/class
-Yummly.configure do |config|
-  config.app_id = "4e9f2aa0"
-  config.app_key = "e54e9cfa05d58b64bab7da2301f05c0b"
-  config.use_ssl = true # Default is false
+require 'yummly'
+
+class Search
+  attr_accessor :results
+
+  def initialize
+    Yummly.configure do |config|
+      config.app_id = "4e9f2aa0"
+      config.app_key = "e54e9cfa05d58b64bab7da2301f05c0b"
+      config.use_ssl = true # Default is false
+    end
+  end
+
+  def search(recipe)
+    @results = Yummly.search(recipe) # this returns an array of recipe results
+  end
+
+  def display_results
+    first_recipe_name = @results.first.name
+    first_recipe_ingredients = @results.first.ingredients # this returns an array of recipe ingredients
+    first_recipe_id = @results.first.id
+
+    puts "--------------------------------"
+    puts "Recipe name: #{first_recipe_name}"
+    puts "Recipe ingredients: #{first_recipe_ingredients}"
+    puts "Here is a link to the recipe: http://www.yummly.com/recipe/#{first_recipe_id}"
+    puts "--------------------------------"
+  end
 end
 
-# this searches Yummly for a recipe and returns an array of recipe objects
-# this should go into a method within your new model/class
-results = Yummly.search('type of recipe you want') #=> an array of recipe results
+searcher = Search.new
+searcher.search('chocolate chip cookies')
+searcher.display_results
