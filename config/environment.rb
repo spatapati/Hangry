9f1dee152780089832f7e5a9e9461ccbcfa7e945
Fkastenbaum <fskast@gmail.com>
2015-07-29 15:32:52 +0000

    updated environment

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@ require 'bundler'
 Bundler.require
 
 require './app/models/model.rb'
-#require './app/models/user'
+require './app/models/recipe.rb'
 
 configure :development do
   set :database, "sqlite3:db/database.db"
